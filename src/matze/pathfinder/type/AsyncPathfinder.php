68550351946961639d4c73010248dee5d402ae47
Matze997 <matthias.kuegele@gmail.com>
2023-06-19 10:27:56 +0200

    AsyncPathfinder: Add isRunning method

--- src/matze/pathfinder/type/AsyncPathfinder.php
+++ src/matze/pathfinder/type/AsyncPathfinder.php
@@ -13,6 +13,8 @@ use pocketmine\Server;
 use pocketmine\world\World;
 
 class AsyncPathfinder implements IPathfinder {
+    private bool $running = false;
+
     public function __construct(
         protected Settings $settings,
         protected World $world,
@@ -20,6 +22,18 @@ class AsyncPathfinder implements IPathfinder {
     ){}
 
     public function findPath(Vector3 $startVector, Vector3 $targetVector, Closure $onCompletion): void {
-        Server::getInstance()->getAsyncPool()->submitTask(new AsyncPathfinderTask($startVector->asVector3(), $targetVector->asVector3(), $this->settings, $this->world, $onCompletion, $this->chunkCacheLimit));
+        if($startVector->floor()->equals($targetVector->floor())) {
+            ($onCompletion)(null);
+            return;
+        }
+        $this->running = true;
+        Server::getInstance()->getAsyncPool()->submitTask(new AsyncPathfinderTask($startVector->asVector3(), $targetVector->asVector3(), $this->settings, $this->world, function (mixed $result) use ($onCompletion): void {
+            ($onCompletion)($result);
+            $this->running = false;
+        }, $this->chunkCacheLimit));
+    }
+
+    public function isRunning(): bool {
+        return $this->running;
     }
 }
